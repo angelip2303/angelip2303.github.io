4a476a165488f1485a773b98155fb42d81260b2a
angelip2303 <angel.iglesias.prestamo@gmail.com>
2022-07-14 00:36:59 +0200

    fixing the navbar on low res

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,42 @@
+import React from "react";
+
 import "./Header.css";
 
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes, FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function Header(): JSX.Element {
+  const [isMenuToggled, setMenuToggled] = React.useState(true);
+
+  const toggleMenu = () => {
+    document.getElementById("menu")?.classList.toggle("active");
+    setMenuToggled((current) => !current);
+  };
+
   return (
     <header>
       <h1>aip</h1>
-      <label htmlFor="toggle">
+      <div className="toggle-menu" hidden={!isMenuToggled} onClick={toggleMenu}>
         <FaBars />
-      </label>
-      <input type="checkbox" id="toggle" />
-      <nav>
+      </div>
+      <nav id="menu">
+        <div
+          className="toggle-menu close-menu"
+          hidden={isMenuToggled}
+          onClick={toggleMenu}
+        >
+          <FaTimes />
+        </div>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
         <a href="#contact">Contact</a>
+        <div className="social-media">
+          <a href="https://www.linkedin.com/in/%C3%A1ngel-iglesias-pr%C3%A9stamo-007b561b7/">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/angelip2303">
+            <FaGithub />
+          </a>
+        </div>
       </nav>
     </header>
   );
