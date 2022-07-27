f4b2aa7c3c06baea0c5c14d5877c21e2f859aa26
angelip2303 <angel.iglesias.prestamo@gmail.com>
2022-07-28 00:50:39 +0200

    adding some particles

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,15 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import particlesConfig from "./config/particlesConfig";
+
 export default function App(): JSX.Element {
+  const particlesInit = async (main: any) => {
+    await loadFull(main);
+  };
+
   return (
     <React.Fragment>
       <Hero />
@@ -18,6 +26,103 @@ export default function App(): JSX.Element {
       <Projects />
       <Contact />
       <Footer />
+
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        params={{
+          particles: {
+            number: {
+              value: 160,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: "#ffffff",
+            },
+            shape: {
+              type: "circle",
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0,
+                sync: false,
+              },
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 4,
+                size_min: 0.3,
+                sync: false,
+              },
+            },
+            line_linked: {
+              enable: false,
+              distance: 150,
+              color: "#ffffff",
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 1,
+              direction: "none",
+              random: true,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 600,
+              },
+            },
+          },
+          interactivity: {
+            events: {
+              onclick: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                line_linked: {
+                  opacity: 1,
+                },
+              },
+              bubble: {
+                distance: 250,
+                size: 0,
+                duration: 2,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.2,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
+          },
+          retina_detect: true,
+        }}
+      />
     </React.Fragment>
   );
 }
