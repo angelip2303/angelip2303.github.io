c9091d6411c4fc70a340bc0d1dff0dfc560baa2c
angelip2303 <angel.iglesias.prestamo@gmail.com>
2022-11-24 19:53:23 +0100

    overall tweaks

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -9,9 +9,17 @@ export default function ThemeSelector(): JSX.Element {
   const [isDarkMode, setDarkMode] = React.useState(false);
 
   const toggleDarkMode = () => {
-    document.documentElement.className = isDarkMode ? "light" : "dark";
+    const mode: string = isDarkMode ? "light" : "dark";
+    localStorage.setItem("theme", mode);
+    document.documentElement.className = mode;
     setDarkMode((mode) => !mode);
   };
+
+  React.useEffect(() => {
+    const localStorageTheme = localStorage.getItem("theme"); // TODO: refactor use react context :D
+    const mode: string = localStorageTheme ? localStorageTheme : "light";
+    document.documentElement.className = mode;
+  }, []);
 
   return (
     <a onClick={toggleDarkMode}>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,7 +4,7 @@ import projects from "../projects.json";
 
 export default function Projects(): JSX.Element {
   return (
-    <section id="events">
+    <section id="projects">
       <List title={"The projects I have worked on"} list={projects.projects} />
     </section>
   ); // TODO: remove projects.projects => just projects
