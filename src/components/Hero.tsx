753c6064fa6c91ef99b2f93fb74fa05c07fa5f83
angelip2303 <angel.iglesias.prestamo@gmail.com>
2022-11-25 13:41:27 +0100

    improving the texts

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,12 +12,13 @@ export default function Hero(): JSX.Element {
       </h1>
 
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis
-        leo sit amet nibh tristique venenatis. Nunc orci erat, tristique eget
-        eros eget, pharetra faucibus velit. Curabitur mattis dictum maximus.
-        Pellentesque vitae orci efficitur, pharetra est nec, sollicitudin magna.
-        Phasellus aliquet sem nunc. Nunc vitae nisi sit amet mauris imperdiet
-        posuere. Sed vitae risus volutpat mi tristique ultricies et ac sapien.
+        I&apos;m a Software Engineering student from the University of Oviedo,
+        here at Spain. My interest in web development started back in 2014 when
+        I started to create custom themes for Wordpress. That experience taught
+        me a lot about HTML and CSS. Fast-forward to today, and I&apos;m
+        currently in my last year of the degree. My main focus these days is
+        learning about front-end development as well as I have a high interest
+        in functional programming and programming language design.
       </p>
     </section>
   );
