bb69853b56647a3533ff1261ae3610fe87caf594
angelip2303 <angel.iglesias.prestamo@gmail.com>
2022-11-25 12:59:04 +0100

    badges implemented :D

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -13,6 +13,7 @@ export default function Projects(props: ListProps): JSX.Element {
   return (
     <section className="listSection">
       <h1>{props.title}</h1>
+      <br />
       <ol className="list">
         {props.list.map((element) => (
           <ListItem listItem={element as ListItemType} key={element.name} />
