85f990182ea0468aa79507fd800c103b6a34ce83
angelip2303 <angel.iglesias.prestamo@gmail.com>
2023-01-25 12:47:17 +0100

    fixing issues regarding the version of astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
-export default defineConfig({
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
-});
+export default defineConfig(
+  /** @type {import('astro').AstroUserConfig} */ {
+    site: "https://angelip2303.github.io/",
+    integrations: [tailwind(), image()],
+  }
+);
