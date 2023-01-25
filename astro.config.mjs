000bcd5d5e4a182f162aff5a368f690e252446a4
angelip2303 <angel.iglesias.prestamo@gmail.com>
2023-01-25 13:30:37 +0100

    trying to fix the build version

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
-export default defineConfig(
-  /** @type {import('astro').AstroUserConfig} */ {
-    site: "https://angelip2303.github.io/",
-    integrations: [tailwind(), image()],
-  }
-);
+export default defineConfig({
+  site: "https://angelip2303.github.io/",
+  integrations: [tailwind(), image()],
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   darkMode: ["class"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
